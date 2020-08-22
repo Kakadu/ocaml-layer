f11bca21d63a1e500f826c3c7e5dc1b9d628daec
Kakadu <Kakadu@pm.me>
2020-08-22 19:21:54 +0300

    Fix building image from template
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- configs/ubuntu.sh
+++ configs/ubuntu.sh
@@ -16,10 +16,10 @@ from="ubuntu"
 
 # This is the argument of 'docker pull', 'docker push', etc. for the image
 # we are building.
-docker_url="mjambon/ocaml:ubuntu"
+docker_url="kakadu18/ocaml:fp2020"
 
 # User to create and use. If it already exists, we'll try to use it.
-user="user"
+user="buildbot"
 
 # Extra packages to be installed by the native package manager.
 extra_packages="$extra_deb_packages"
