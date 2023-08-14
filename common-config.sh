407b827d6420f16debca9e65713d9f43fdb900f7
Kakadu <Kakadu@pm.me>
2023-08-14 22:13:14 +0300

    Add new packages and commands in the end
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- common-config.sh
+++ common-config.sh
@@ -8,14 +8,15 @@ extra_apk_packages="
 
 # Ubuntu
 extra_deb_packages="
-  pkg-config npm nasm
+  pkg-config npm nasm autoconf
 "
 
 # The collection of opam packages we want to install. Go wild.
 opam_packages="
-  dune.3.4.1
-  ocamlformat.0.23.0
-	ppxlib.0.26.0
+  dune.3.10.0
+  ocamlformat.0.26.0
+	ppxlib.0.30.0
+	bisect_ppx.2.8.3
 	sexplib
 	yojson
 	stdune
@@ -23,10 +24,15 @@ opam_packages="
   base
   ppx_inline_test
   ppx_expect
-	ppx_assert
-  angstrom
+  ppx_show
+	angstrom
+  opal
   qcheck-core
   ppx_deriving
 	stdune
 	sexplib
+  curly
+  zanuda
 "
+
+# curly is required for reviewer
