54b82c319ad694fef877aca55f95b1a505e56920
Dmitrii Kosarev a.k.a. Kakadu <Kakadu@pm.me>
2023-09-30 19:40:39 +0300

    WIP on Ubuntu 23.04
    Signed-off-by: Dmitrii Kosarev a.k.a. Kakadu <Kakadu@pm.me>

--- common-config.sh
+++ common-config.sh
@@ -8,10 +8,16 @@ extra_apk_packages="
 
 # Ubuntu
 extra_deb_packages="
+  nasm clang-14 curl ca-certificates
+  autoconf cmake llvm-14-dev python3
+  adduser
 "
 
 # The collection of opam packages we want to install. Go wild.
 opam_packages="
-  dune
-  utop
+  dune.3.10.0
+  ocamlformat.0.26.1
+  dune-site angstrom ppx_blob ppx_show llvm
+  ppx_expect
+  llvm.14.0.6
 "
--- configs/ubuntu.sh
+++ configs/ubuntu.sh
@@ -25,11 +25,11 @@ os="ubuntu"
 # by gpgv.
 #
 #
-from="ubuntu:20.04"
+from="ubuntu:23.04"
 
 # This is the argument of 'docker pull', 'docker push', etc. for the image
 # we are building.
-docker_url="mjambon/ocaml:ubuntu"
+docker_url="kakadu18/ocaml:llvm14"
 
 # User to create and use. If it already exists, we'll try to use it.
 user="user"
