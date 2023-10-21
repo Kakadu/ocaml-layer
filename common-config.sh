c948ae850f91eb059f5f43fafb26ea99332b465f
Dmitrii Kosarev a.k.a. Kakadu <Kakadu@pm.me>
2023-10-21 16:01:43 +0300

    Add more LLVM packages
    Signed-off-by: Dmitrii Kosarev a.k.a. Kakadu <Kakadu@pm.me>

--- common-config.sh
+++ common-config.sh
@@ -12,18 +12,25 @@ extra_deb_packages="
   rlwrap
   libgmp-dev
   llvm-14 cmake libffi-dev llvm-14-dev
+  autoconf
 "
 
 # The collection of opam packages we want to install. Go wild.
 opam_packages="
-  dune.3.6.1
+  dune.3.11.0
   ppxlib
   ppx_expect
   ppx_show
   ppx_inline_test
-	ppx_blob
-	angstrom
+  ppx_blob
+  ppx_deriving
+  bisect_ppx
+  angstrom
   ctypes
   ctypes-foreign
   llvm.14.0.6
+  yojson
+  curly
+  parsexp.v0.16.0
+  sexplib.v0.16.0
 "
