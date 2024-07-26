07554456c94b0a6dbcec024d4441ebc1f28fdc5c
Kakadu <Kakadu@pm.me>
2024-07-27 00:12:27 +0300

    Prepare for fp2024
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- common-config.sh
+++ common-config.sh
@@ -8,16 +8,17 @@ extra_apk_packages="
 
 # Ubuntu
 extra_deb_packages="
-  pkg-config npm nasm autoconf
+  pkg-config nasm autoconf
 "
 
 # The collection of opam packages we want to install. Go wild.
 opam_packages="
-  dune.3.11.1
-  ocamlformat.0.26.1
-	ppxlib.0.30.0
+  dune.3.16.0
+  ocamlformat.0.26.2
+	ppxlib.0.32.1
 	bisect_ppx.2.8.3
   odoc
+  odig
 	sexplib
   base
 	stdune
