5d9452584b668de98267e87c3326a9d5d7ddd769
Kakadu <Kakadu@pm.me>
2024-08-07 21:55:59 +0300

    FP2024 prepared
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- common-config.sh
+++ common-config.sh
@@ -8,30 +8,30 @@ extra_apk_packages="
 
 # Ubuntu
 extra_deb_packages="
-  pkg-config nasm autoconf
+  pkg-config nasm autoconf python-is-python3
 "
 
 # The collection of opam packages we want to install. Go wild.
 opam_packages="
   dune.3.16.0
   ocamlformat.0.26.2
-	ppxlib.0.32.1
-	bisect_ppx.2.8.3
+  ppxlib.0.32.1
+  bisect_ppx.2.8.3
   odoc
   odig
-	sexplib
+  sexplib
   base
-	stdune
+  stdune
   ppx_inline_test
   ppx_expect
   ppx_show
   ppx_deriving
-	angstrom
+  angstrom
   opal
   qcheck-core
-	sexplib
+  sexplib
   curly
-	yojson
+  yojson
   zanuda
 "
 
