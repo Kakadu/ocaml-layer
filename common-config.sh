c112e1cfebb8afbfaf0d94ef8660f84100fdf7aa
Kakadu <Kakadu@pm.me>
2025-03-01 18:09:45 +0300

    lama image
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- common-config.sh
+++ common-config.sh
@@ -8,31 +8,17 @@ extra_apk_packages="
 
 # Ubuntu
 extra_deb_packages="
-  pkg-config nasm autoconf python-is-python3
+  pkg-config autoconf python-is-python3 gcc-i686-linux-gnu gcc-14-multilib xdot linux-libc-dev:i386
 "
 
 # The collection of opam packages we want to install. Go wild.
 opam_packages="
-  dune.3.16.0
-  ocamlformat.0.26.2
+  opam-depext
+  dune.3.17.2
   ppxlib.0.32.1
-  bisect_ppx.2.8.3
-  odoc
-  odig
-  sexplib
-  base
-  stdune
-  ppx_inline_test
-  ppx_expect
-  ppx_show
-  ppx_deriving
-  angstrom.0.16.0
-  opal
-  qcheck-core
-  sexplib
-  curly
-  yojson
-  zanuda
+  GT
+  ostap
+  posix-uname
 "
 
 # curly is required for reviewer
