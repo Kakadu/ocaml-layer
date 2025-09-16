21b14b0945b084703afc95083c963ab68badc8b0
Kakadu <Kakadu@pm.me>
2025-09-16 21:40:14 +0300

    It works
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- common-config.sh
+++ common-config.sh
@@ -10,38 +10,28 @@ extra_apk_packages="
 extra_deb_packages="
   adduser python-is-python3 python3 curl ca-certificates autoconf
   libzstd-dev zlib1g-dev
-  cmake llvm-18-dev llvm-18-runtime clang-18 gcc-13
-  gcc-riscv64-linux-gnu g++-riscv64-linux-gnu qemu-user
-  binutils pkg-config
+  pkg-config
 "
 
 # The collection of opam packages we want to install. Go wild.
 opam_packages="
   dune.3.19.1
-  ocamlformat.0.27.0
-  dune-site
-  ppx_expect
-  llvm.18-shared
-  ctypes-foreign
+  yojson
+  sexplib
+  dune-build-info
   odoc
   odig
   bisect_ppx
-  stdune
   ppxlib
-  ppx_expect.v0.16.1
+  ppx_blob
   ppx_show
+  ppx_deriving
+  ppx_expect
+  ppx_expect_nobase.v0.17.2.2
+  ppx_fields_conv
   ppx_inline_test
   ppx_blob
-  ppx_deriving
-  qcheck
-  angstrom.0.16.0
-  ctypes
-  ctypes-foreign
-  yojson
+  menhir
   curly
-  odoc
-  parsexp.v0.16.0
-  sexplib.v0.16.0
-  ppx_expect_nobase.v0.17.2.2
-  mtime.2.1.0
+  ocamlformat.0.26.2
 "
--- postinstall.sh
+++ postinstall.sh
@@ -5,3 +5,10 @@ cd
 
 opam clean
 
+du -sh ~/.opam
+find $(opam var bin) -iname '*.byte' -exec rm -fv {} \;
+find $(opam var bin) -iname 'ocamlformat.parser_reconver.test*' -exec rm -f {} \;
+find $(opam var lib) -iname '*.cmt' -o -iname '*.cmti' -exec rm -f {} \;
+find $(opam var prefix) -iname '*.exe' -exec strip {} \;
+du -sh ~/.opam
+
