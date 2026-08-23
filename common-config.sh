e7efca32ea779b803337f0eb7cfc623b67969237
Kakadu <kakadu.hafanana@gmail.com>
2026-08-24 00:59:40 +0300

    Melange OCanren 5.5
    Signed-off-by: Kakadu <kakadu.hafanana@gmail.com>

--- common-config.sh
+++ common-config.sh
@@ -11,30 +11,13 @@ extra_deb_packages="
   adduser python-is-python3 python3 curl ca-certificates autoconf
   libzstd-dev zlib1g-dev
   pkg-config automake autoconf libtool
-  ncdu time
+  ncdu time npm
 "
 
 # The collection of opam packages we want to install. Go wild.
 opam_packages="
   dune.3.24.2
-  yojson
-  jsonschema
-  sexplib
-  dune-build-info
-  odoc
-  odig
-  angstrom.0.16.0
-  #bisect_ppx
-  ppxlib
-  ppx_blob
-  #ppx_show
-  ppx_deriving
-  ppx_expect
-  ppx_expect_nobase.0.17.3.1
-  ppx_fields_conv
-  ppx_inline_test
-  menhir
-  curly
+  melange
   ocamlformat
-  ocaml-syntax-shims
+  opam-check-npm-deps 
 "
